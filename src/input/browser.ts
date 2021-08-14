303a0063635b87d87af7476dd99dcbe1c0de6433
A Goblin King <agoblinking@pm.me>
2021-08-13 18:25:34 -0700

    More clear about SAB

--- src/input/browser.ts
+++ src/input/browser.ts
@@ -3,6 +3,6 @@ import { Value } from 'src/util/value'
 export const url = new Value(window.location.pathname.slice(1).split('/'))
 export const hasSharedArrayBuffer = window.SharedArrayBuffer !== undefined
 
-if (!hasSharedArrayBuffer) {
-  document.body.classList.add('nosab')
+if (hasSharedArrayBuffer) {
+  document.body.classList.add('sab')
 }
