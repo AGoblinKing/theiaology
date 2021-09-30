9a03b396c5d907d00e47d1d5d1e272105c0e26fb
A Goblin King <agoblinking@pm.me>
2021-09-29 20:05:34 -0700

    more steam iteration

--- src/steam.ts
+++ src/steam.ts
@@ -1,28 +1,21 @@
 import { Value } from './value'
-interface IGreenworks {
-  [key: string]: any
-}
-export const steam = new Value<IGreenworks>()
 
+interface IGreenworks extends EventTarget {
+  saves: []
+  workshop: []
+  post: (data) => void
+}
 // @ts-ignore
-if (window.require !== undefined) {
-  try {
-    steam.set(window.require('greenworks.js'))
-    const $s = steam.$
-    $s.init()
-    $s.SteamStart()
-  } catch (ex) {
-    console.error(ex)
-  }
-}
-
-function SteamStart() {
-  // do all the things
-}
+export const steam = new Value<IGreenworks>(window.steam)
 
 export const steam_open = new Value<string[]>([]).re((arr) => {
-  if (arr) {
-    const [url, where] = arr
-    //steam.$.openURL(url, where)
+  if (!arr || !steam.$) return
+  const [url, where] = arr
+
+  if (where === '_self') {
+    window.open(url, '_self')
+    return
   }
+
+  steam.$.post(`open|${url}|${where}`)
 })
