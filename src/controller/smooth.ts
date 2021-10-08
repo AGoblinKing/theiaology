00f26141587f9b297ace9751599da5b7ff285ea1
A Goblin King <agoblinking@pm.me>
2021-10-07 18:56:53 -0700

    better passive move noise

--- src/controller/smooth.ts
+++ src/controller/smooth.ts
@@ -1,12 +1,12 @@
 // move body smoothly
 
 import { body, camera, renderer } from 'src/render'
-import { delta, timing } from 'src/shader/time'
+import { delta, Timer, timing } from 'src/shader/time'
 import { Value } from 'src/value'
 import { Vector3 } from 'three'
 import { MIDI } from './audio'
 
-export const MIN_VELOCITY = 0.1
+export const MIN_VELOCITY = 0.3
 
 export const velocity = new Value(new Vector3(0, 0, 0))
 export const angular = new Value(0)
@@ -27,9 +27,6 @@ timing.on(($t) => {
         renderer.xr.isPresenting ? camera.quaternion : body.$.quaternion
       )
     )
-
-    if (i++ % 2 === 0) return
-    MIDI(84, 40 + (i % 2), 0.2 + l * 0.005)
   }
 
   if (Math.abs(angular.$) > MIN_VELOCITY) {
@@ -39,4 +36,17 @@ timing.on(($t) => {
 
     angular.$ -= angleta
   }
+})
+
+Timer(500, () => {
+  if (i++ % 2 === 0) return
+
+  const l = Math.abs(velocity.$.length())
+  if (l < MIN_VELOCITY) return
+
+  // heart
+  MIDI(81, 30 + ((i % 2) + (i % 5)), 0.25 + l * 0.01)
+  setTimeout(() => {
+    MIDI(81, 30 + ((i % 2) + (i % 5)), 0.25 + l * 0.01)
+  }, 100)
 })
